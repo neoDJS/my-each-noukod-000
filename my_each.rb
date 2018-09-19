6ef7a099343e703bf6440c16d7a6ba931bbf6baf
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-09-19 19:34:39 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(arr) # put argument(s) here
   # code here
+  if block_given?
+    arr.length.times do |i|
+      yield arr[i]
+    end
+  else
+    puts "Hey! No block was given!"
+  end
 end
